8dc6139502a10e66316f21db6ed86d391b17fe8e
babayota <babayota@gmail.com>
2026-08-08 14:14:27 +0300

    Update aircon_climate.h
    corrected Fahrenheit/Celsius report indoor_temperature_setting/status for some AEH-W4A1 units as in https://github.com/pslawinski/esphome_airconintl/blob/main/components/airconintl/aircon_climate.h
    strings 152-172

--- components/airconintl/aircon_climate.h
+++ components/airconintl/aircon_climate.h
@@ -149,9 +149,21 @@ namespace esphome
                             ((Device_Status *)uart_buf)->left_right,
                             ((Device_Status *)uart_buf)->up_down);
 
-                        // Convert temperatures to celsius (fixed swapped current/target values)
-                        float tgt_temp = (((Device_Status *)uart_buf)->indoor_temperature_status - 32) * 0.5556f;
-                        float curr_temp = (((Device_Status *)uart_buf)->indoor_temperature_setting - 32) * 0.5556f;
+                        // Some AEH-W4A1 units report indoor_temperature_setting/status in
+                        // Fahrenheit, others already in Celsius. Use the same temperature_unit
+                        // flag that already controls which command templates (C or F) are sent,
+                        // so both variants are handled correctly.
+                        float tgt_temp, curr_temp;
+                        if (temperature_unit == "F")
+                        {
+                            tgt_temp = (((Device_Status *)uart_buf)->indoor_temperature_setting - 32) * 0.5556f;
+                            curr_temp = (((Device_Status *)uart_buf)->indoor_temperature_status - 32) * 0.5556f;
+                        }
+                        else
+                        {
+                            tgt_temp = ((Device_Status *)uart_buf)->indoor_temperature_setting;
+                            curr_temp = ((Device_Status *)uart_buf)->indoor_temperature_status;
+                        }
 
                         if (tgt_temp > 7 && tgt_temp < 33)
                             target_temperature = tgt_temp;
