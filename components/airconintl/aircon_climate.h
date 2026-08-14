b23bb9b042eafe41e50467abc0966882eee78f15
babayota <babayota@gmail.com>
2026-08-14 11:21:37 +0300

    Update aircon_climate.h
    correct size of message to 50

--- components/airconintl/aircon_climate.h
+++ components/airconintl/aircon_climate.h
@@ -716,31 +716,16 @@ namespace esphome
             // Set the temperature
             void set_temp(float temp)
             {
-                if (temperature_unit == "C")
+                // Примусово використовуємо Цельсій для вибору c_messages
+                uint8_t temp_c = roundf(temp);
+                if (temp_c >= 16 && temp_c <= 32)
                 {
-                    uint8_t temp_c = roundf(temp);
-                    if (temp_c >= 16 && temp_c <= 32)
-                    {
-                        int index = temp_c - 16;
-                        // Беремо строго 49 байтів для Цельсія
-                        std::vector<uint8_t> msg(temp_c_messages[index], temp_c_messages[index] + 49);
-                        snprintf(desc_buffer, sizeof(desc_buffer), "Set Temperature to %d°C", temp_c);
-                        ESP_LOGD("aircon_climate", "Enqueuing %s", desc_buffer);
-                        send_message(desc_buffer, msg);
-                    }
-                }
-                else
-                {
-                    uint8_t temp_f = roundf(temp * 1.8f + 32);
-                    if (temp_f >= 61 && temp_f <= 86)
-                    {
-                        int index = temp_f - 61;
-                        // Беремо строго 49 байтів для Фаренгейта
-                        std::vector<uint8_t> msg(temp_f_messages[index], temp_f_messages[index] + 49);
-                        snprintf(desc_buffer, sizeof(desc_buffer), "Set Temperature to %d°F", temp_f);
-                        ESP_LOGD("aircon_climate", "Enqueuing %s", desc_buffer);
-                        send_message(desc_buffer, msg);
-                    }
+                    int index = temp_c - 16;
+                    // Передаємо всі 50 байтів (з урахуванням кінцевого 0xFB)
+                    std::vector<uint8_t> msg(temp_c_messages[index], temp_c_messages[index] + 50);
+                    snprintf(desc_buffer, sizeof(desc_buffer), "Set Temperature to %d°C", temp_c);
+                    ESP_LOGD("aircon_climate", "Enqueuing %s", desc_buffer);
+                    send_message(desc_buffer, msg);
                 }
             }
         };
